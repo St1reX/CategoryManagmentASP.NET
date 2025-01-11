0ed5f61d02af74c128220ff91a9eefbae6e093f5
Jakub Uryga <urygajakub@gmail.com>
2025-01-11 17:19:30 +0100

    Dodaj pliki projektów.

---
+++ CategoryManagmentASP.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CategoryManagmentASP.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CategoryManagmentASP.NET.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
