ad4e9557be855350b7f963b61c94112736277ea3
Jakub Uryga <urygajakub@gmail.com>
2025-01-12 16:23:46 +0100

    complete category managment

--- CategoryManagmentASP.NET/Controllers/CategoryController.cs
+++ CategoryManagmentASP.NET/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using CategoryManagmentASP.NET.Data;
+using CategoryManagmentASP.NET.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CategoryManagmentASP.NET.Controllers
@@ -12,9 +13,103 @@ namespace CategoryManagmentASP.NET.Controllers
             _db = db;
         }
 
+        //GET
         public IActionResult Index()
         {
             return View(_db.Categories.ToList());
+        }
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Category name can't match exactly display order");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryToEdit = _db.Categories.Find(id);
+            if(categoryToEdit == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(categoryToEdit);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Category name can't match exactly display order");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryToDelete = _db.Categories.Find(id);
+            if (categoryToDelete == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(categoryToDelete);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var categoryToDelete = _db.Categories.Find(id);
+            _db.Categories.Remove(categoryToDelete);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
