1b4328ffaa93ee3777db2b9062860265068591eb
Jakub Uryga <urygajakub@gmail.com>
2025-01-12 11:58:02 +0100

    category view + controller basics + custom theme

---
+++ CategoryManagmentASP.NET/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using CategoryManagmentASP.NET.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoryManagmentASP.NET.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Categories.ToList());
+        }
+    }
+}
